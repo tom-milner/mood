11aea92b3d6db816c02d6e192ca284b0234af830
tom-milner <tom.f.milner@gmail.com>
2019-03-03 17:20:32 +0000

    refactored scenarios page

--- lib/pages/scenarios/scenarios.dart
+++ lib/pages/scenarios/scenarios.dart
@@ -1,27 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:mood_app/pages/scenarios/PageContent.dart';
 import "package:mood_app/widgets/PageTitle.dart";
-import "package:mood_app/pages/scenarios/MoodCard.dart";
 
 class ScenariosPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
     return Container(
       padding: EdgeInsets.all(10.0),
-        child: new Column(
+      child: new Column(
 //          mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              new PageTitle("SCENARIOS"),
-              new SizedBox(height: 50.0,),
-              new Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-              new MoodCard("School", Icons.note),
-              new MoodCard("Relationships", Icons.favorite_border),
-              new MoodCard("Family", Icons.group)],)
-            ],
-        ),
+          children: <Widget>[
+            new PageTitle("SCENARIOS"),
+            new SizedBox(
+//              height: 50.0,
+            ),
+             new PageContent(),
+          ]),
     );
   }
 }
--- lib/ui/theme.dart
+++ lib/ui/theme.dart
@@ -1,20 +1,20 @@
 import "package:flutter/material.dart";
 
 final ThemeData MoodThemeData = new ThemeData(
-  // default brightness and colours
-  brightness: Brightness.dark,
-  // 152,204,211
-  primaryColor: Color.fromRGBO(245, 245, 245, 1),
+    // default brightness and colours
+    brightness: Brightness.dark,
+    // 152,204,211
+    primaryColor: Color.fromRGBO(245, 245, 245, 1),
 //  accentColor: Color.fromRGBO(58, 66, 86, 1.0),
-  canvasColor: Color.fromRGBO(229, 229, 229, 1.0),
-  buttonColor: Color.fromRGBO(84, 152, 245, 1.0),
-  // default font
-  fontFamily: "Muli"
-  // Text themes
+    canvasColor: Color.fromRGBO(229, 229, 229, 1.0),
+    buttonColor: Color.fromRGBO(84, 152, 245, 1.0),
+    // default font
+    fontFamily: "Muli"
+    // Text themes
 //  textTheme: TextTheme(
 //    headline: TextStyle(fontSize: )
 //  )
-);
+    );
 
 //   primaryColor: Color.fromRGBO(224, 233, 245, 1),
 ////  accentColor: Color.fromRGBO(58, 66, 86, 1.0),
