3086ba176a8860e75e764d0b0dcb819502383a66
tom-milner <tom.f.milner@gmail.com>
2019-03-03 17:36:46 +0000

    fixed merge conflicts

--- lib/pages/scenarios/MoodCard.dart
+++ lib/pages/scenarios/MoodCard.dart
@@ -32,7 +32,7 @@ class _MoodCardState extends State<MoodCard>{
   @override
   Widget build(BuildContext context) {
 
-      Card(
+      return Card(
       elevation: 2.0,
       margin: EdgeInsets.symmetric(horizontal: 5.0, vertical: 6.0),
         shape: RoundedRectangleBorder(
