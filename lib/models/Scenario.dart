10c92eebd362f83304399d906d71247aaf862d45
tom-milner <tom.f.milner@gmail.com>
2019-04-04 08:16:01 +0100

    updated Scenario model

--- lib/models/Scenario.dart
+++ lib/models/Scenario.dart
@@ -1,27 +1,29 @@
 import 'package:flutter/material.dart';
 
 class Scenario {
-
+  final int id = 0;
   final String title ;
   final String content ;
   final String icon ;
-//  final DocumentReference reference;
 
 
   Scenario(this.title, this.content, this.icon);
 
 //
   Scenario.fromMap(Map<String, dynamic> map,)
-    :assert(map["title"] != null),
-  assert(map["content"] != null),
-  assert(map["icon"] != null),
-  title = map["title"],
+//    :assert(map["title"] != null),
+//  assert(map["id"] != null),
+//  assert(map["content"] != null),
+//  assert(map["icon"] != null),
+  :title = map["title"],
   content = map["content"],
   icon = map["icon"];
+//  id = map["id"];
 
 
-//  Scenario.fromSnapshot(DocumentSnapshot snapshot)
-//    :this.fromMap(snapshot.data, reference: snapshot.reference);
+
+  Scenario.fromSnapshot(AsyncSnapshot snapshot)
+    :this.fromMap(snapshot.data);
 
   @override
   String toString() => "Scenario<$title,$content>";
