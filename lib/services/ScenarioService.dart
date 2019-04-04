e45d69d403394b48dc4a341de04e7e403e900a0b
tom-milner <tom.f.milner@gmail.com>
2019-04-04 08:16:33 +0100

    updated scenario service

--- lib/services/ScenarioService.dart
+++ lib/services/ScenarioService.dart
@@ -1,23 +1,42 @@
 import "package:mood_app/models/Scenario.dart";
 import "package:mood_app/services/database/DatabaseService.dart";
 import "package:sqflite/sqflite.dart";
-class ScenariosService {
 
-
-  final String tableName = "Scenarios";
-  DatabaseService dbService;
+class ScenarioService {
+  String tableName = "Scenarios";
+  DatabaseService dbService = DatabaseService.dbService;
   Database db;
 
-  init() async{
-    dbService =  DatabaseService.db;
-    db = await dbService.getDatabase(tableName);
-  }
-
   Future<List<Scenario>> getAllScenarios() async {
+    await newScenario();
+
+    final db = await dbService.database;
     // get whole Scenarios table
     var res = await db.query(tableName);
-    if(!res.isNotEmpty) return res.map((scenario) => Scenario.fromMap(scenario)).toList();
+    if (!res.isNotEmpty)
+      print(res.map((scenario) => Scenario.fromMap(scenario)).toList());
+      return res.map((scenario) => Scenario.fromMap(scenario)).toList();
     return [];
   }
 
+  newScenario() async {
+    final db = await dbService.database;
+    
+//    print(await db.rawQuery("Select * FROM Scenarios"));
+
+
+    print(await db.query("Scenarios"));
+    var table = await db.rawQuery("SELECT MAX(scenarioId)+1 as scenarioId FROM Scenarios");
+
+    print(table);
+
+    int id = table.first["id"];
+
+    var raw = await db.rawInsert(
+        "INSERT into Scenarios (scenarioId,title,content,icon)"
+        "Values (?,?,?,?)",
+        [id, "34242", "vcsdvfdf", "fcdsfgs"]);
+
+    return raw;
+  }
 }
