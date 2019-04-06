f7ae06cc21685f17d8d9fa16a46aaa3058c1377b
tom-milner <tom.f.milner@gmail.com>
2019-04-06 18:28:06 +0100

    added testing data

--- lib/services/ScenarioService.dart
+++ lib/services/ScenarioService.dart
@@ -1,12 +1,31 @@
+import 'dart:math';
+
 import "package:mood_app/models/Scenario.dart";
 import "package:mood_app/services/database/DatabaseService.dart";
 import "package:sqflite/sqflite.dart";
+
+
+// testing data
+class DummyData{
+  List<Scenario> dummyScenarios = [
+  Scenario("hello", "csfdvdfv", "dfcrkdfv"),
+  Scenario("goodbye", "csfdvdfv", "dfcrkdfv"),
+  Scenario("hi", "csfdvdfv", "dfcrkdfv"),
+  Scenario("mental", "csfdvdfv", "dfcrkdfv"),
+  Scenario("health", "csfdvdfv", "dfcrkdfv"),
+    ];
+}
+
 
 class ScenarioService {
   String tableName = "Scenarios";
   DatabaseService dbService = DatabaseService.dbService;
   Database db;
 
+  
+ 
+  
+  
   Future<List<Scenario>> getAllScenarios() async {
     await newScenario();
 
@@ -27,15 +46,17 @@ class ScenarioService {
 
     print(await db.query("Scenarios"));
     var table = await db.rawQuery("SELECT MAX(scenarioId)+1 as scenarioId FROM Scenarios");
-
-    print(table);
-
     int id = table.first["id"];
+
+    List<Scenario> data = DummyData().dummyScenarios;
+    Random random = new Random();
+    // get random entry in dummy data
+    var randomEntry = data[random.nextInt(data.length - 1)];
 
     var raw = await db.rawInsert(
         "INSERT into Scenarios (scenarioId,title,content,icon)"
         "Values (?,?,?,?)",
-        [id, "34242", "vcsdvfdf", "fcdsfgs"]);
+        [id, randomEntry.title, randomEntry.content, randomEntry.icon]);
 
     return raw;
   }
