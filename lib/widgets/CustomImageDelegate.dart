b706d44a47335a0568dd0522e2f080a5d448fcec
tom-milner <tom.f.milner@gmail.com>
2019-05-26 12:35:33 +0100

    imager_picker integration in events

--- lib/widgets/CustomImageDelegate.dart
+++ lib/widgets/CustomImageDelegate.dart
@@ -8,9 +8,11 @@ import 'package:image_picker/image_picker.dart';
 import 'package:zefyr/zefyr.dart';
 
 class CustomImageDelegate implements ZefyrImageDelegate<ImageSource> {
+
   @override
   Future<String> pickImage(ImageSource source) async {
-    final file = await ImagePicker.pickImage(source: source);
+    final file = await ImagePicker.pickImage(
+        source: source);
     if (file == null) return null;
     // We simply return the absolute path to selected file.
     return file.uri.toString();
